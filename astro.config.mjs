05a8da9bf541dee23ae56bd75718e97d03553bf0
Alexander Bifulco <alex@capturemedia.ch>
2025-06-22 16:12:05 +0200

    chore: page done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     outDir: 'docs',
     build: {
         assets: 'astro_'
+    },
+    vite: {
+        plugins: [tailwindcss()]
     }
 });
